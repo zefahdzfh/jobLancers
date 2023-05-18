e5e82d7ce0b61fbe9dda79adf7b6cc335e092e08
zefahdzfh <zefakalibata@gmail.com>
2023-05-18 13:08:15 +0700

    page 1 90%

--- public/JobLancer.js
+++ public/JobLancer.js
@@ -1,8 +1,9 @@
 // -----typed.js-----
 
 var typed = new Typed('.typedjs', {
-    strings: ["Connecting You with", "The Best Freelance Opportunities."],
-    typeSpeed: 60,
-    backSpeed: 50,
+    strings: [ "Welcome to the official website of Joblancers!", "أهلاً وسهلاً بك في موقع جوبلانسرز الرسمي!" ,"Benvenuto sul sito web ufficiale di Joblancers!"
+  , "Willkommen auf der offiziellen Website von Joblancers!", "Bienvenue sur le site web officiel de Joblancers !","¡Bienvenido al sitio web oficial de Joblancers!" ,"ジョブランサーズ公式ウェブサイトへようこそ！","欢迎光临Joblancers官方网站！","Joblancers 공식 웹사이트에 오신 것을 환영합니다! "],
+    typeSpeed: 40,
+    backSpeed: 60,
     loop: true,
   });
